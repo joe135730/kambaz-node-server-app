ab4bd8da234d7815ea06c152c11c22310414687c
Cheyi Wu <wu.chey@northeastern.edu>
2025-03-31 19:55:24 -0400

    Initial commit

---
+++ Lab5/module/Module.js
@@ -0,0 +1,24 @@
+const module = {
+  id: 1,
+  name: "NodeJS module",
+  description: "Create a NodeJS server with ExpressJS",
+  course: "CS5610",
+};
+export default function Module(app) {
+  app.get("/lab5/module", (req, res) => {
+    res.json(module);
+  });
+  app.get("/lab5/module/name", (req, res) => {
+    res.json(module.name);
+  });
+  app.get("/lab5/module/name/:newName", (req, res) => {
+    const { newName } = req.params;
+    module.name = newName;
+    res.json(module);
+  });
+  app.get("/lab5/module/description/:newDescription", (req, res) => {
+    const { newDescription } = req.params;
+    module.description = newDescription;
+    res.json(module);
+  });
+}
---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import Hello from './Hello.js'
+import Lab5 from "./Lab5/index.js";
+import cors from "cors";
+
+const app = express() // create new express 
+app.use(cors());
+app.use(express.json());
+Lab5(app);
+Hello(app) // pass app reference to Hello
+
+app.listen(process.env.PORT || 4000) // listen to http://localhost:4000
+
+
+
+
+
