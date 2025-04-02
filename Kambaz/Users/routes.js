e0726956aa1fb521b28c46d1f0413e1990dc0db2
Cheyi Wu <wu.chey@northeastern.edu>
2025-04-01 23:07:55 -0400

    5.3.2.4

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -35,15 +35,22 @@ export default function UserRoutes(app) {
     currentUser = dao.findUserByCredentials(username, password);
     res.json(currentUser);
   };
+  app.post("/api/users/signin", signin);
+
+  const profile = async (req, res) => {
+    res.json(currentUser);
+  };
+
   const signout = (req, res) => {};
-  const profile = (req, res) => {};
+
+
 
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
 
   app.delete("/api/users/:userId", deleteUser);
   app.post("/api/users/signup", signup);
-  app.post("/api/users/signin", signin);
+
   app.post("/api/users/signout", signout);
   app.post("/api/users/profile", profile);
 }
