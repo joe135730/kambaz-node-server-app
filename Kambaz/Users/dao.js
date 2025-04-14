d468ff6768c0bc4252bb2b564e2ff31692bc3a64
Cheyi Wu <wu.chey@northeastern.edu>
2025-04-14 02:33:29 -0400

    6.2.6.7

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -2,11 +2,11 @@ import db from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
 import model from "./model.js";
 
-let { users } = db;
+// let { users } = db;
 export const createUser = (user) => {
   const newUser = { ...user, _id: uuidv4() };
-  users = [...users, newUser];
-  return newUser;
+  return model.create(newUser);
+
 };
 export const findAllUsers = () => model.find();
 export const findUserById = (userId) => model.findById(userId);
