3cbf95cb863ff33964b1364595be04f06934cc05
Cheyi Wu <wu.chey@northeastern.edu>
2025-04-15 16:24:03 -0400

    6.4.3.1

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,35 +1,16 @@
-import Database from "../Database/index.js";
-import { v4 as uuidv4 } from "uuid";
+import model from "./model.js";
 
-export function enrollUserInCourse(userId, courseId) {
-  const { enrollments } = Database;
-  // Check if enrollment already exists
-  const existingEnrollment = enrollments.find(
-    e => e.user === userId && e.course === courseId
-  );
-  if (existingEnrollment) {
-    throw new Error("User is already enrolled in this course");
-  }
-  // Create new enrollment
-  const newEnrollment = { _id: uuidv4(), user: userId, course: courseId };
-  Database.enrollments.push(newEnrollment);
-  return newEnrollment;
+export async function findCoursesForUser(userId) {
+ const enrollments = await model.find({ user: userId }).populate("course");
+ return enrollments.map((enrollment) => enrollment.course);
 }
-
-export function unenrollUserFromCourse(userId, courseId) {
-  const { enrollments } = Database;
-  const index = enrollments.findIndex(
-    e => e.user === userId && e.course === courseId
-  );
-  if (index === -1) {
-    throw new Error("User is not enrolled in this course");
-  }
-  // Remove the enrollment
-  Database.enrollments.splice(index, 1);
-  return { success: true };
+export async function findUsersForCourse(courseId) {
+ const enrollments = await model.find({ course: courseId }).populate("user");
+ return enrollments.map((enrollment) => enrollment.user);
 }
-
-export function findUserEnrollments(userId) {
-  const { enrollments } = Database;
-  return enrollments.filter(enrollment => enrollment.user === userId);
+export function enrollUserInCourse(user, course) {
+ return model.create({ user, course, _id: `${user}-${course}` });
+}
+export function unenrollUserFromCourse(user, course) {
+ return model.deleteOne({ user, course });
 }
